38ca4c565c4c243d7a59753abf66705a7ad739e0
Shreya Khanduri <131514715+Khanduri004@users.noreply.github.com>
2025-07-08 13:18:57 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
-from .routes import main
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+db = SQLAlchemy()  # Initialize here
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
+
+    #  Lazy import to avoid circular imports
+    from .routes import main
     app.register_blueprint(main)
+
     return app
