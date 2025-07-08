c65c4a9692b6e7e4adda51ed983bc2aa4d4f2e55
Shreya Khanduri <131514715+Khanduri004@users.noreply.github.com>
2025-07-08 13:21:05 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,37 +1,10 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, request, jsonify
 from .models import Product, CartItem
 from . import db
 
-@main.before_app_first_request
-def create_tables():
-    db.create_all()
-    if not Product.query.first():
-        db.session.add_all([
-            Product(name='Phone', price=499.99),
-            Product(name='Laptop', price=899.99),
-            Product(name='Shoes', price=59.99),
-        ])
-        db.session.commit()
-
 main = Blueprint('main', __name__)
 
-@main.route('/')
-def index():
+@main.route('/products')
+def get_products():
     products = Product.query.all()
-    return render_template('index.html', products=products)
-
-@main.route('/add/<int:product_id>')
-def add_to_cart(product_id):
-    item = CartItem.query.filter_by(product_id=product_id).first()
-    if item:
-        item.quantity += 1
-    else:
-        item = CartItem(product_id=product_id, quantity=1)
-        db.session.add(item)
-    db.session.commit()
-    return redirect('/cart')
-
-@main.route('/cart')
-def view_cart():
-    cart_items = CartItem.query.all()
-    return render_template('cart.html', cart=cart_items)
+    return jsonify([{'name': p.name, 'price': p.price} for p in products])
