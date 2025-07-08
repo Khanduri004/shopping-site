8156a57eaa7f8579287a9b736a3f79c11cad0df7
Shreya Khanduri <131514715+Khanduri004@users.noreply.github.com>
2025-07-08 14:21:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from app import create_app
+# app.py
 
-app = create_app()
+from flask import Flask
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route('/')
+def index():
+    return "Hello from Flask!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
