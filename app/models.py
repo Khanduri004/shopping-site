d816c551999e3095396daa26c257267bd5251880
Shreya Khanduri <131514715+Khanduri004@users.noreply.github.com>
2025-07-08 12:13:35 +0100

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from . import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Float)
+
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    quantity = db.Column(db.Integer)
